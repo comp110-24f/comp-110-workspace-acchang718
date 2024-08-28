799355c691cbf1a339f2897efffb54b966ef599b
acchang718 <acchang@ad.unc.edu>
2024-08-28 09:48:13 -0400

    Challenge Question 00 done

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,18 @@
+"""Challenge Question 00"""
+
+__author__ = "730481718"
+
+
+def mimic(message: str) -> str:
+    """Returns a message to mimic"""
+    return message
+
+
+def main() -> None:
+    """Calls mimic"""
+    print(mimic(message=input("What is your message?")))
+    return None
+
+
+if __name__ == "__main__":
+    main()
